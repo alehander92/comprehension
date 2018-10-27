ceb3721022e12933307d75730efb3ef377f31a22
Alexander Ivanov <alehander42@gmail.com>
2018-10-27 23:53:07 +0300

    Change to {}

--- src/comprehension.nim
+++ src/comprehension.nim
@@ -1,31 +1,20 @@
 import macros, tables, sets, typetraits, sequtils
 
-proc fixHeader(f: NimNode): NimNode =
-  expectKind(f, nnkForStmt)
-  expectKind(f[2], nnkCall)
+
+proc compImpl(f: NimNode, isSet: static[bool]): NimNode =
   result = f
-  result[2] = f[2][1]
-
-type
-  ResultComp = enum SeqComp, TableComp, SetComp
-
-
-{.experimental: "forLoopMacros".}
-
-# Faith
-macro comp*(f: ForLoopStmt): untyped =
-  result = fixHeader(f)
   var code = result[^1]
-  if code.kind in {nnkStmtList, nnkStmtListExpr}:
+  while code.kind in {nnkStmtList, nnkStmtListExpr}:
     if code.len > 1:
       error "expected 1 child"
     else:
       code = code[0]
-  else:
-    result[^1] = nnkStmtList.newTree(code)
+  
+  result[^1] = nnkStmtList.newTree(code)
 
   var z = code
   var isTest = false
+  echo code.lisprepr
   case code.kind:
   of nnkIfStmt:
     z = code[0][1]
@@ -33,7 +22,6 @@ macro comp*(f: ForLoopStmt): untyped =
   else:
     discard
   
-  var comp = SeqComp
   var newCode = z
   var pre = nnkStmtList.newTree()
   var compResult = genSym(nskVar)
@@ -42,7 +30,6 @@ macro comp*(f: ForLoopStmt): untyped =
 
   case z.kind:
   of nnkTableConstr:
-    comp = TableComp
     let key = z[0][0]
     let value = z[0][1]
     var aValue = nnkForStmt.newTree(result[0], result[1], result[2], result[3])
@@ -54,24 +41,25 @@ macro comp*(f: ForLoopStmt): untyped =
       var `compResult` = initTable[`keyType`, `valueType`]()
     newCode = quote:
       `compResult`[`key`] = `value`
-  of nnkCurly:
-    comp = SetComp
-    let child = z[0]
-    a[^1] = child
-    let childType = newCall(bindSym"type", a)
-    pre = quote:
-      var `compResult` = initSet[`childType`]()
-    newCode = quote:
-      `compResult`.incl(`child`)
+  # of nnkCurly:
+  # 
   else:
-    comp = SeqComp
-    let child = z
-    a[^1] = child
-    let childType = newCall(bindSym"type", a)
-    pre = quote:
-      var `compResult` = newSeq[`childType`]()
-    newCode = quote:
-      `compResult`.add(`child`)
+    if isSet:
+      let child = z
+      a[^1] = child
+      let childType = newCall(bindSym"type", a)
+      pre = quote:
+        var `compResult` = initSet[`childType`]()
+      newCode = quote:
+        `compResult`.incl(`child`)
+    else:
+      let child = z
+      a[^1] = child
+      let childType = newCall(bindSym"type", a)
+      pre = quote:
+        var `compResult` = newSeq[`childType`]()
+      newCode = quote:
+        `compResult`.add(`child`)
   
   if isTest:
     result[^1][0][0][1] = newCode
@@ -84,15 +72,31 @@ macro comp*(f: ForLoopStmt): untyped =
       `result`
       `compResult`
 
-  # echo result.repr
+  echo result.repr
+
+# Faith
+macro comp*(f: untyped): untyped =
+  compImpl(f, false)
+
+macro `{}`*(c: untyped, f: untyped): untyped =
+  if c.repr == "comp":
+    result = compImpl(f, true)
+  else:
+    result = newLit("2")
 
 when isMainModule:
   let a = {0: 0, 3: 0}.toTable()
 
-  let e = (for k, v in comp(a): (if k == v: {k}))
-  echo e
-  let f = (for k, v in comp(a): (if k == v: {k: v}))
-  echo f
+  # let e = comp(for k, v in a: (if k == v: {k}))
+  # echo e
+  # let f = comp(for k, v in a: (if k == v: {k: v}))
+  # echo f
 
-  let g = (for k, v in comp(a): k + v)
-  echo g
+  # let g = comp(for k, v in a: k + v)
+  # echo g
+
+  echo comp{for e, f in a: 0}
+  echo comp[for e, f in a: 0]
+  echo comp{for e, f in a: {e:f}}
+
+
--- tests/test_comprehension.nim
+++ tests/test_comprehension.nim
@@ -6,14 +6,14 @@ let a = {0: 0, 3: 0}.toTable()
 
 suite "comprehension":
   test "set from table":
-    let e = (for k, v in comp(a): (if k == v: {k}))
+    let e = comp{for k, v in a: (if k == v: k)}
     check(e == @[0].toSet())
 
   test "table from table":
-    let f = (for k, v in comp(a): (if k == v: {k: v}))
+    let f = comp{for k, v in a: (if k == v: {k: v})}
     check(f == {0: 0}.toTable())
 
   test "seq from table":
-    let g = (for k, v in comp(a): k + v)
+    let g = comp[for k, v in a: k + v]
     check(g == @[0, 3])
 
